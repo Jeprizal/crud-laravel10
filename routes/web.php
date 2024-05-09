93ac375c190b3272506994218b61662735e0f043
jeprizal <jeprizal1901@gmail.com>
2024-05-09 20:09:28 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use app\Http\Controller\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//route resource
+Route::resource('/posts', \App\Http\Controllers\PostController::class);
+
+// memastikan penambahan route berhasil
+// php artisan route:list
